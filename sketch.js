74a013a5d5d48676cbac5749c27738aaf7584b6f
rubani-k <97441057+rubani-k@users.noreply.github.com>
2022-01-14 19:13:39 +0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,53 @@
+var sea,ship;
+var seaImg,shipImg;
+var shark, sharkImg;
+
+function preload(){
+  //uncomment the code to add animation to ship 
+
+  shipImg1 = loadAnimation("ship-1.png");
+  shipImg1 = loadAnimation("ship-1.png");
+  //shipImg1 = loadAnimation("ship-1");
+  //shipImg1 = loadAnimation("ship-1.png","ship-2.png","ship-1.png","ship-2.png");
+  //shipImg1 = loadAnimation("ship-1","ship-2","ship-1","ship-2");
+  
+  seaImg = loadImage("sea.png");
+
+  sharkImg = loadImage("download-removebg-preview.png")
+}
+
+function setup(){
+  createCanvas(400,400);
+  background("blue");
+
+  // Moving background
+  sea=createSprite(400,200);
+  sea.addImage(seaImg);
+  sea.velocityX = -5;
+  sea.scale=0.3;
+
+  
+  ship = createSprite(130,200,30,30);
+  ship.addAnimation("movingShip",shipImg1);
+  ship.scale =0.25;
+  
+  shark = createSprite(130,350,20,20);
+  shark.addAnimation("shark",sharkImg);
+  shark.scale = 0.5;
+}
+
+function draw() {
+  background(0);
+  sea.velocityX = -3;
+
+  //uncomment code to reset the background
+  if(sea.x < 0){
+    //sea.x = 0;
+    //sea.x = sea.width;
+    sea.x = sea.width/8;
+    //sea.y = height;
+  }
+
+ 
+  drawSprites();
+}
